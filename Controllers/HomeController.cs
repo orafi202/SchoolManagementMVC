dd12835656d15b7c2a45206f2d2d6b2a3cb5e31a
Muhammad Rafi <orafi202@gmail.com>
2023-03-25 23:38:01 +0500

    About Page is added in the view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,10 +18,17 @@ namespace SchoolManagementMVC.Controllers
             return View();
         }
 
+        
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
